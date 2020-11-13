ef08d26ab06a1a46103ecf54e01259a267eb323a
the-tunnster <therealtarunkrishnan@gmail.com>
2020-11-13 15:38:21 +0530

    Added a function and implementation for a varation of the program.

--- GoLang/EasyMode/NamePrint.go
+++ GoLang/EasyMode/NamePrint.go
@@ -9,3 +9,15 @@ func namePrint(){
 	fmt.Scanf("%s%s", &firstName, &lastName)
 	fmt.Printf("The name entered was %s %s.\n", firstName, lastName)
 }
+
+func specificNamePrint(){
+	var firstName string
+	var testName string = "Alice"
+	fmt.Println("Please enter your firstname.")
+	fmt.Scanf("%s", &firstName)
+	if(firstName==testName){
+		fmt.Printf("How's it going, %s?\n", firstName)
+	}else{
+		fmt.Println("Hello.")
+	}
+}
