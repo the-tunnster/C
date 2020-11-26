b9811a35e3ea5892edd0c9ef7fc0ad766ae7bc0e
the-tunnster <therealtarunkrishnan@gmail.com>
2020-11-26 22:58:59 +0530

    Wrote a program to print multiplication tables.

---
+++ GoLang/EasyMode/MultiplicationTables.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func tablePrinter(){
+	for i:=1;i<=12;i++{
+		for j:=1;j<=10;j++{
+			fmt.Printf("%d times %d is %d.\n", i, j, (i*j))
+		}
+	fmt.Println("******************")
+	}
+}
