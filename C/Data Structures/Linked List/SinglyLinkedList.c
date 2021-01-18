257e14b8bf9d5c1dd10ba1c32c804ff847314720
the-tunnster <therealtarunkrishnan@gmail.com>
2021-01-18 13:05:57 +0530

    Changed some formatting, it was annoying me.

--- C/Data Structures/Linked List/SinglyLinkedList.c
+++ C/Data Structures/Linked List/SinglyLinkedList.c
@@ -7,25 +7,25 @@ struct node{
 };
 
 
-struct node * CreateNewNode(int value);
+struct node* CreateNewNode(int);
 struct node* DeleteANode(struct node *);
 struct node* InsertAtBeginning(struct node *, struct node *);
 struct node* InsertAtTheEnd(struct node *, struct node *);
 struct node* InsertInTheMiddle(struct node *, struct node *, int);
 struct node* DeleteInTheBeginning(struct node *);
 struct node* DeleteInTheEnd(struct node *);
-struct node* DeleteInTheMiddle(struct node *, int n);
+struct node* DeleteInTheMiddle(struct node *, int);
 struct node* InsertANode(struct node *);
 struct node* DeleteANode(struct node *);
 
-int main() {
+int main(){
     struct node * head = NULL;
     int n;
     do{
         printf("Would you like to add or remove a node?\n");
         printf("1 to add, 2 to remove.\n");
         scanf("%d",&n);
-        if(n==1) {
+        if(n==1){
             head=InsertANode(head);
         }
         else{
@@ -35,35 +35,35 @@ int main() {
     return 0;
 }
 
-struct node * CreateNewNode(int value){
+struct node* CreateNewNode(int value){
     struct node *ptr = (struct node *)malloc(sizeof(struct node));
     ptr->info=value;
     ptr->next=NULL;
     return(ptr);
 }
 
-struct node* InsertANode(struct node *head) {
+struct node* InsertANode(struct node *head){
     printf("Enter the value to put in the node.\n");
     int value;
     scanf("%d", &value);
     struct node *PTR = CreateNewNode(value);
-    if (PTR == NULL) {
+    if(PTR == NULL){
         printf("Overflow condition achieved. Exiting...");
     }
     else{
         printf("Where would you like to insert a node?\n1. The beginning.\n2. The end.\n3. The middle.\n");
         int ch;
         scanf("%d", &ch);
-        switch (ch) {
-            case 1: {
+        switch(ch){
+            case 1:{
                 head=InsertAtBeginning(head, PTR);
                 break;
             }
-            case 2: {
+            case 2:{
                 head=InsertAtTheEnd(head, PTR);
                 break;
             }
-            case 3: {
+            case 3:{
                 printf("At what position would you like it to be?\n");
                 int n;
                 scanf("%d", &n);
@@ -99,22 +99,23 @@ struct node* InsertAtTheEnd(struct node *head, struct node *ptr){
 }
 
 struct node* DeleteANode(struct node * head) {
-    if (head == NULL) {
+    if(head == NULL) {
         printf("Underflow condition achieved. Exiting...");
-    } else {
+    }
+    else{
         printf("From where would you like to delete a node?\n1. The beginning.\n2. The end.\n3. The middle.\n");
         int ch;
         scanf("%d", &ch);
-        switch (ch) {
-            case 1: {
+        switch(ch){
+            case 1:{
                 head=DeleteInTheBeginning(head);
                 break;
             }
-            case 2: {
+            case 2:{
                 head=DeleteInTheEnd(head);
                 break;
             }
-            case 3: {
+            case 3:{
                 printf("At what position would you like it to be removed from?\n");
                 int n;
                 scanf("%d", &n);
