d7d8b3084c2f3416f9a7baa5363fea6f5a5be0d0
the-tunnster <therealtarunkrishnan@gmail.com>
2021-01-27 11:46:57 +0530

    Added way to set different number of nodes/edges.

--- C/Graphs/Kruskal.c
+++ C/Graphs/Kruskal.c
@@ -4,12 +4,12 @@
 
 typedef struct edge {
   int u, v, w;
-}edge;
+} edge;
 
-typedef struct edge_list{
+typedef struct edge_list {
   edge data[MAX];
   int n;
-}edge_list;
+} edge_list;
 
 edge_list elist;
 
@@ -22,14 +22,13 @@ void applyUnion(int belongs[], int c1, int c2);
 void sort();
 void print();
 
-// Applying Krushkal Algo
 void kruskalAlgo(){
-  int belongs[MAX], cno1, cno2;
+  int belongs[MAX], i, j, cno1, cno2;
   elist.n = 0;
 
-  for(int i = 1; i < n; i++)
-    for(int j = 0; j < i; j++){
-      if(Graph[i][j] != 0) {
+  for (i = 1; i < n; i++)
+    for (j = 0; j < i; j++) {
+      if (Graph[i][j] != 0) {
         elist.data[elist.n].u = i;
         elist.data[elist.n].v = j;
         elist.data[elist.n].w = Graph[i][j];
@@ -39,12 +38,12 @@ void kruskalAlgo(){
 
   sort();
 
-  for (int i = 0; i < n; i++)
+  for (i = 0; i < n; i++)
     belongs[i] = i;
 
   spanlist.n = 0;
 
-  for (int i = 0; i < elist.n; i++) {
+  for (i = 0; i < elist.n; i++) {
     cno1 = find(belongs, elist.data[i].u);
     cno2 = find(belongs, elist.data[i].v);
 
@@ -69,11 +68,12 @@ void applyUnion(int belongs[], int c1, int c2) {
 }
 
 // Sorting algo
-void sort(){
+void sort() {
+  int i, j;
   edge temp;
 
-  for(int i = 1; i < elist.n; i++)
-    for (int j = 0; j < elist.n - 1; j++)
+  for (i = 1; i < elist.n; i++)
+    for (j = 0; j < elist.n - 1; j++)
       if (elist.data[j].w > elist.data[j + 1].w) {
         temp = elist.data[j];
         elist.data[j] = elist.data[j + 1];
@@ -82,7 +82,7 @@ void sort(){
 }
 
 // Printing the result
-void print(){
+void print() {
   int i, cost = 0;
 
   for (i = 0; i < spanlist.n; i++) {
@@ -90,61 +90,30 @@ void print(){
     cost = cost + spanlist.data[i].w;
   }
 
-  printf("\nSpanning tree cost: %d", cost);
+  printf("\nSpanning tree cost: %d.\n", cost);
 }
 
-int main() {
+int main(){
   int i, j, total_cost;
-
   n = 6;
 
-  Graph[0][0] = 0;
-  Graph[0][1] = 4;
-  Graph[0][2] = 4;
-  Graph[0][3] = 0;
-  Graph[0][4] = 0;
-  Graph[0][5] = 0;
-  Graph[0][6] = 0;
+  printf("How many vertices do you have in your graph?.\n");
+  int numOfVertices;
+  scanf("%d", &numOfVertices);
 
-  Graph[1][0] = 4;
-  Graph[1][1] = 0;
-  Graph[1][2] = 2;
-  Graph[1][3] = 0;
-  Graph[1][4] = 0;
-  Graph[1][5] = 0;
-  Graph[1][6] = 0;
-
-  Graph[2][0] = 4;
-  Graph[2][1] = 2;
-  Graph[2][2] = 0;
-  Graph[2][3] = 3;
-  Graph[2][4] = 4;
-  Graph[2][5] = 0;
-  Graph[2][6] = 0;
-
-  Graph[3][0] = 0;
-  Graph[3][1] = 0;
-  Graph[3][2] = 3;
-  Graph[3][3] = 0;
-  Graph[3][4] = 3;
-  Graph[3][5] = 0;
-  Graph[3][6] = 0;
-
-  Graph[4][0] = 0;
-  Graph[4][1] = 0;
-  Graph[4][2] = 4;
-  Graph[4][3] = 3;
-  Graph[4][4] = 0;
-  Graph[4][5] = 0;
-  Graph[4][6] = 0;
-
-  Graph[5][0] = 0;
-  Graph[5][1] = 0;
-  Graph[5][2] = 2;
-  Graph[5][3] = 0;
-  Graph[5][4] = 3;
-  Graph[5][5] = 0;
-  Graph[5][6] = 0;
+  for(int i=0; i<numOfVertices; i++){
+    for(int j=i+1;j<numOfVertices; j++){
+      if(i==j){
+        Graph[i][j] = 0;
+        continue;
+      }
+      printf("Enter the cost from %d to %d.\n", i, j);
+      int temp;
+      scanf("%d", &temp);
+      Graph[i][j] = temp;
+      Graph[j][i] = temp;
+    }
+  }
 
   kruskalAlgo();
   print();
