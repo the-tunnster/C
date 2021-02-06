bed810e1184cf89037cc63fcc47d56e95c62230c
the-tunnster <therealtarunkrishnan@gmail.com>
2021-02-06 11:28:46 +0530

    Removed comments and restructured the code a bit.

--- C/Graphs/DepthFirstSearch.c
+++ C/Graphs/DepthFirstSearch.c
@@ -1,73 +1,64 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 
-struct list //adjacency list
-{
+struct list{
     int v;
     struct list *next;
 } **edge; 
-void init(int N)
-{
+
+void init(int N){
     for(int i=0;i<N;i++)
         edge[i]=NULL;
 }
-void addEdge(int u, int v)
-{
+
+void addEdge(int u, int v){
     struct list *tmp=(struct list*)malloc(sizeof(struct list));
     tmp->v=v;
     tmp->next=NULL;
-
     if(edge[u]==NULL)
         edge[u]=tmp;
-    else
-    {
+    else{
         tmp->next=edge[u];
         edge[u]=tmp;
     }
 }
-void dfs(int i, int vis[])
-{
-    vis[i]=1;
 
+void dfs(int i, int vis[]){
+    vis[i]=1;
     struct list *tmp=edge[i];
-    while (tmp!=NULL)
-    {
+    while (tmp!=NULL){
         if(!vis[tmp->v])
             dfs(tmp->v,vis);
         tmp=tmp->next;
     }
 }
-int main() //main method
-{
-    int N=0,M,u,v;
 
+int main(){
+    int N=0,M,u,v;
     printf("Enter the number of vertices and the number of edge\n");
     scanf("%d%d",&N,&M);
-
     edge=(struct list**)malloc(N*sizeof(struct list*));
     init(N);
 
-    for(int i=0;i<M;i++)
-    {
+    for(int i=0;i<M;i++){
         printf("Enter an edge\n");
         scanf("%d%d",&u,&v);
-        u--;v--;
-
+        u--;
+        v--;
         addEdge(u,v);
         addEdge(v,u);
     }
 
     int vis[N];
-    for(int i=0;i<N;i++) vis[i]=0;
+    for(int i=0;i<N;i++)
+        vis[i]=0;
 
     dfs(0,vis);
-
     int flag=1;
-    for(int i=0;i<N;i++) flag&=vis[i];
+    for(int i=0;i<N;i++)
+        flag&=vis[i];
 
     printf("\nThe graph is: ");
     printf(flag?"Connected\n":"Not connected\n");
-
     return 0;
 }
